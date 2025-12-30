803fd4ca5dfba7453efc182a5e65e180dd1e9069
saksham gupta <guptasaksham2510@gmail.com>
2025-12-30 20:32:21 +0530

    implemented entities of the hotel-service

---
+++ hotel-service/src/main/java/com/hotel/hotelservice/entity/Hotel.java
@@ -0,0 +1,56 @@
+package com.hotel.hotelservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "hotels")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(length = 1000)
+    private String description;
+
+    
+//    Email of the manager assigned to this hotel
+//    Comes from auth-service
+     
+    @Column(nullable = false)
+    private String managerEmail;
+
+    
+//     Amenities stored as comma-separated values
+//     Example: WIFI,POOL,PARKING
+    
+    @Column(length = 1000)
+    private String amenities;
+
+    
+//   One hotel can have multiple room categories
+     
+    @OneToMany(
+            mappedBy = "hotel",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<RoomCategory> roomCategories;
+}
--- hotel-service/src/main/java/com/hotel/hotelservice/entity/RoomCategory.java
+++ hotel-service/src/main/java/com/hotel/hotelservice/entity/RoomCategory.java
@@ -1,5 +1,52 @@
 package com.hotel.hotelservice.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+    name = "room_categories",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"hotel_id", "category"})
+    }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoomCategory {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+//     STANDARD, DELUXE, SUITE, etc.
+     
+    @Column(nullable = false)
+    private String category;
+
+
+//      Total number of rooms for this category
+//      Used by booking-service for Redis availability
+
+    @Column(nullable = false)
+    private Integer totalRooms;
+
+    
+//    Max number of guests allowed per room
+     
+    @Column(nullable = false)
+    private Integer capacity;
+
+    
+//    Base price per night
+     
+    @Column(nullable = false)
+    private Double basePrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
 }
