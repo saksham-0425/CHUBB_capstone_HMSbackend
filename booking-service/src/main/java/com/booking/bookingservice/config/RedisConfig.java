994e36ca28f6c9a0fb1736e719ede0e237401e57
saksham gupta <guptasaksham2510@gmail.com>
2025-12-31 21:29:19 +0530

    redis configuration for availability

--- booking-service/src/main/java/com/booking/bookingservice/config/RedisConfig.java
+++ booking-service/src/main/java/com/booking/bookingservice/config/RedisConfig.java
@@ -3,22 +3,15 @@ package com.booking.bookingservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Integer> redisTemplate(
+    public StringRedisTemplate stringRedisTemplate(
             RedisConnectionFactory connectionFactory
     ) {
-        RedisTemplate<String, Integer> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-
-        return template;
+        return new StringRedisTemplate(connectionFactory);
     }
 }
