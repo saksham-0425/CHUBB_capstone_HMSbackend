ecc09dd55eefdeef241d8948d3962013c7dca34b
saksham gupta <guptasaksham2510@gmail.com>
2026-01-03 00:08:16 +0530

    configuration for rabbitMQ

--- booking-service/src/main/java/com/booking/bookingservice/config/BookingRabbitMQConfig.java
+++ booking-service/src/main/java/com/booking/bookingservice/config/BookingRabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.booking.bookingservice.config;
 
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,4 +17,21 @@ public class BookingRabbitMQConfig {
     public TopicExchange bookingEventsExchange() {
         return new TopicExchange(BOOKING_EXCHANGE, true, false);
     }
+    
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter
+    ) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+    
 }
